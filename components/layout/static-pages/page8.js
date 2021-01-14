bf4113bfc2058d80d3c00279f83092fabdf44e5e
mluena <maluenarod@gmail.com>
2021-01-14 13:55:23 +0100

    empty static pages added

---
+++ components/layout/static-pages/page8.js
@@ -0,0 +1,16 @@
+const Page8 = () => (
+        <div className="wrapper">
+            <div className="row">
+                <div className="col-xs-4">
+                 
+                </div>
+                <div className="col-xs-8">
+
+                   
+                </div>
+            </div>
+
+    </div>
+);
+
+export default Page8;
