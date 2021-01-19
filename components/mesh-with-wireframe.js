78aafaf36db32cfc04d603cbb128496d94742df0
Alvaro Leal <pursuance@gmail.com>
2021-01-19 14:06:34 +0100

    More width to lines

--- components/mesh-with-wireframe.js
+++ components/mesh-with-wireframe.js
@@ -3,32 +3,31 @@ import { useUpdate, extend } from 'react-three-fiber';
 import { WireframeGeometry2 } from 'three/examples/jsm/lines/WireframeGeometry2';
 import { Wireframe } from 'three/examples/jsm/lines/Wireframe';
 import { LineMaterial } from 'three/examples/jsm/lines/LineMaterial';
-
 import * as THREE from 'three';
 
 extend({ LineMaterial, WireframeGeometry2, Wireframe });
 
 function MeshWithWireframe({
-  mesh, geometry, position, rotation, color, renderOrder
+  mesh, geometry, position, rotation, color, renderOrder, linewidth
 }) {
   const lines = useUpdate((geo) => geo.fromEdgesGeometry(new THREE.EdgesGeometry(geometry)), []);
   const resolutionRatio = 1;
   const resolution = 256 * resolutionRatio;
   const otherProps = { position, rotation, renderOrder };
   return (
-  <>
-    {mesh}
-    <wireframe {...otherProps}>
-      <wireframeGeometry2 attach="geometry" ref={lines}/>
-      <lineMaterial
-        attach="material"
-        color={color || "white"}
-        linewidth={resolutionRatio / 5}
-        resolution={[resolution, resolution]}
-      />
-    </wireframe>
-  </>
+    <>
+      {mesh}
+      <wireframe {...otherProps}>
+        <wireframeGeometry2 attach="geometry" ref={lines}/>
+        <lineMaterial
+          attach="material"
+          color={color || "white"}
+          linewidth={resolutionRatio / 5 * (linewidth || 1)}
+          resolution={[resolution, resolution]}
+        />
+      </wireframe>
+    </>
   );
-}
+};
 
 export default MeshWithWireframe;
