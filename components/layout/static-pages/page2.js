98f99518eb4be732a82a1533aedd1af09fbd4d76
mluena <maluenarod@gmail.com>
2021-02-05 11:17:38 +0100

    texts and links, imgs deleted

--- components/layout/static-pages/page2.js
+++ components/layout/static-pages/page2.js
@@ -5,7 +5,7 @@ const Page2 = () => (
       <h2>In 2020, a deeper level of fire analysis was added to Global Forest Watch.</h2>
     </div>
     <div className="col-2">
-      <p>More detailed fire data analysis arrived on <a href="https://www.globalforestwatch.org/" target="_blank">Global Forest Watch </a> in time to track the record-breaking USA wildfires. Visitors to the platform could see which states were worst affected, and see if the number of fire alerts in those places was higher than normal. (Spoiler alert, the number of fire alerts in California and Oregon was unusually high in September 2020). The ability to put data into context is vital for understanding what changes are happening and how to respond to them.</p>
+      <p>More detailed fire data analysis arrived on <a className="-strong -underline" href="https://www.globalforestwatch.org/" target="_blank">Global Forest Watch </a> in time to track the record-breaking USA wildfires. Visitors to the platform could see which states were worst affected, and see if the number of fire alerts in those places was higher than normal. (Spoiler alert, the number of fire alerts in California and Oregon was unusually high in September 2020). The ability to put data into context is vital for understanding what changes are happening and how to respond to them.</p>
       <div className="video-container">
         <iframe src="https://www.youtube.com/embed/RqDLNnaNnII"
           frameBorder='0'
@@ -15,7 +15,7 @@ const Page2 = () => (
           title='video'
         />
       </div>
-      <p className="-caption">Wildlands and semi-natural lands have decreased globally since the 18th century. <a href="https://medium.com/vizzuality-blog/where-the-wild-things-arent-7f3d3a994cca" target="_blank" rel="noopener noreferrer">There is however room for hope.</a></p>
+      <p className="-caption">Wildlands and semi-natural lands have decreased globally since the 18th century. <a className="-strong -underline" href="https://medium.com/vizzuality-blog/where-the-wild-things-arent-7f3d3a994cca" target="_blank" rel="noopener noreferrer">There is however room for hope.</a></p>
       <p>We also worked with the Half-Earth Project and Map of Life to publish a new approach to evaluating the level of protection species receive in any given country. The method — which has been called the <a href="https://medium.com/vizzuality-blog/half-earth-national-report-cards-summarise-conservation-efforts-1f3eae6e0ad" target="_blank">Species Protection Index</a>  — will help guide a global biodiversity conservation strategy.</p>
       <p>In 2021, we’ll continue our collaboration with Microsoft and The Nature Conservancy to develop an open-source spatial planning platform based on Marxan. Our role is to make the platform more usable and scalable, thereby increasing the positive impact this tool has on global conservation and sustainable development.</p>
     </div>
