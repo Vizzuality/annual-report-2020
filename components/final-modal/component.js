be0ad77f850fc17a50ac47d37f041e84e8c02738
Alvaro Leal <pursuance@gmail.com>
2021-02-11 12:04:31 +0100

    Update final Download link

--- components/final-modal/component.js
+++ components/final-modal/component.js
@@ -55,7 +55,7 @@ export default function FinalModal({ onClose }) {
             <li>
               <div className="triangle" />
               <a
-                href={'href'}
+                href="https://dl.dropboxusercontent.com/s/lqfsswrof1vl8ju/Spotlight%20on%202020%20by%20vizzuality.pdf"
                 target="_blank"
                 rel="noopener noreferrer"
                 onClick={trackDownloads}
